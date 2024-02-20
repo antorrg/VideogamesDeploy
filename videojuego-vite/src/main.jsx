9f3c13460b5268cc8655a1117c521c1f3236886d
Antonio R. Rodriguez Gramajo <114789901+antorrg@users.noreply.github.com>
2024-02-20 15:02:10 -0300

    Update main.jsx

--- videojuego-vite/src/main.jsx
+++ videojuego-vite/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 import App from "./App";
-
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
+const apiUrl = import.meta.env.VITE_URL;
+
+axios.defaults.baseURL = apiUrl;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
